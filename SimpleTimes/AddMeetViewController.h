bac07560b9e6cf301973c7832f61703c1bd73be3
David LaPorte <david@laporte6.org>
2012-02-18 18:21:46 -0500

    new controller/view for adding a meet

---
+++ SimpleTimes/AddMeetViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddMeetViewController.h
+//  SimpleTimes
+//
+//  Created by David LaPorte on 2/18/12.
+//  Copyright (c) 2012 laporte6.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddMeetViewController : UIViewController
+
+@property (retain) IBOutlet UIDatePicker* date;
+@property (retain) IBOutlet UITextField* name;
+
+@end
---
+++ SimpleTimes/AddMeetViewController.m
@@ -0,0 +1,54 @@
+//
+//  AddMeetViewController.m
+//  SimpleTimes
+//
+//  Created by David LaPorte on 2/18/12.
+//  Copyright (c) 2012 laporte6.org. All rights reserved.
+//
+
+#import "AddMeetViewController.h"
+
+@implementation AddMeetViewController
+
+@synthesize name;
+@synthesize date;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
